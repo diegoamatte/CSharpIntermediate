a714837e5a9dc137b065b13d2a9d054e408749ee
Diego Amatte <damatte@makingsense.com>
2022-02-08 20:31:28 -0300

    Workflow implemented

---
+++ Exercise 6 - Workflow Engine/Workflow.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_6___Workflow_Engine
+{
+    public class Workflow
+    {
+        private List<IActivity> _activities = new();
+
+        public void AddActivity(IActivity activity)
+        {
+            _activities.Add(activity);
+        }
+
+        public void RemoveActivity(IActivity activity)
+        {
+            if (_activities.Contains(activity))
+                _activities.Remove(activity);
+        }
+
+        public IEnumerable<IActivity> GetActivities()
+        {
+            return _activities;
+        }
+    }
+}
---
+++ Exercise 6 - Workflow Engine/WorkflowEngine.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_6___Workflow_Engine
+{
+    public class WorkflowEngine
+    {
+        public void Run(Workflow workflow)
+        {
+            workflow.GetActivities().ToList()
+                .ForEach(activity => activity.Execute());
+        }
+    }
+}
