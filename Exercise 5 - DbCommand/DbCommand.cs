c893f3a4e36dbcd069d5ab4357789295d7096f86
Diego Amatte <damatte@makingsense.com>
2022-02-08 20:29:25 -0300

    Db exercises completed

---
+++ Exercise 5 - DbCommand/DbCommand.cs
@@ -0,0 +1,23 @@
+﻿using Exercise_4___DbConnection;
+
+
+namespace Exercise_5___DbCommand
+{
+    public class DbCommand
+    {
+        private readonly DbConnection _connection;
+        public DbCommand(DbConnection dbConnection)
+        {
+            if (dbConnection == null)
+                throw new ArgumentNullException();
+            _connection = dbConnection;
+        }
+
+        public void Execute()
+        {
+            _connection.OpenConnection();
+            Console.WriteLine("Executing instruction");
+            _connection.CloseConnection();
+        }
+    }
+}
