50c229bd88561345e6a6f1eedc1ed99427e8eb15
Diego Amatte <damatte@makingsense.com>
2022-02-08 20:27:41 -0300

    Exercise 3 completed

---
+++ Exercise 3 - Custom Stack/CustomStack.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_3___Custom_Stack
+{
+    public class CustomStack
+    {
+        private List<Object> _stack = new List<Object>();
+        public void Push(object obj)
+        {
+            _stack.Add(obj);
+        }
+        public object? Pop()
+        {
+            if (_stack.Count > 0)
+            {
+                var value = _stack[_stack.Count - 1];
+                _stack.RemoveAt(_stack.Count -1);
+                return value;
+            }
+                
+            return null;
+        }
+        public void Clear()
+        {
+            _stack.Clear();
+        }
+    }
+}
